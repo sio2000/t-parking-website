e00d509018909d85c9bac4009813be8572efd2ba
sio2000 <karapanosnikos9@gmail.com>
2025-06-12 16:23:21 -0700

    Resolve merge conflicts after pulling from remote

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          motion: ['framer-motion']
+        }
+      }
+    }
+  }
 }) 
